ed141620399199360ce0bd1401c08559c6378897
Denis <denguk@gmail.com>
2019-04-23 13:06:30 +0500

    fix: add content type to result

--- index.js
+++ index.js
@@ -5,6 +5,107 @@ const port = config.port || 4001;
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
+const MIME_TYPES = {
+  html: "text/html; charset=utf-8",
+  htm: "text/html; charset=utf-8",
+  sht: "text/html; charset=utf-8",
+  css: "text/css; charset=utf-8",
+  xml: "text/xml; charset=utf-8",
+  gif: "image/gif",
+  jpeg: "image/jpeg",
+  jpg: "image/jpeg",
+  js: "application/x-javascript; charset=utf-8",
+  atom: "application/atom+xml",
+  rss: "application/rss+xml",
+  mml: "text/mathml",
+  txt: "text/plain",
+  jad: "text/vnd.sun.j2me.app-descriptor",
+  wml: "text/vnd.wap.wml",
+  htc: "text/x-component",
+  png: "image/png",
+  tif: "image/tiff",
+  tiff: "image/tiff",
+  wbmp: "image/vnd.wap.wbmp",
+  ico: "image/x-icon",
+  jng: "image/x-jng",
+  bmp: "image/x-ms-bmp",
+  svg: "image/svg+xml",
+  webp: "image/webp",
+  jar: "application/java-archive",
+  war: "application/java-archive",
+  ear: "application/java-archive",
+  hqx: "application/mac-binhex40",
+  doc: "application/msword",
+  pdf: "application/pdf",
+  ps: "application/postscript",
+  eps: "application/postscript",
+  ai: "application/postscript",
+  rtf: "application/rtf",
+  xls: "application/vnd.ms-excel",
+  ppt: "application/vnd.ms-powerpoint",
+  wmlc: "application/vnd.wap.wmlc",
+  kml: "application/vnd.google-earth.kml+xml",
+  kmz: "application/vnd.google-earth.kmz",
+  '7z': "application/x-7z-compressed",
+  cco: "application/x-cocoa",
+  jardiff: "application/x-java-archive-diff",
+  jnlp: "application/x-java-jnlp-file",
+  run: "application/x-makeself",
+  pl: "application/x-perl",
+  pm: "application/x-perl",
+  prc: "application/x-pilot",
+  pdb: "application/x-pilot",
+  rar: "application/x-rar-compressed",
+  rpm: "application/x-redhat-package-manager",
+  sea: "application/x-sea",
+  swf: "application/x-shockwave-flash",
+  sit: "application/x-stuffit",
+  tcl: "application/x-tcl",
+  tk: "application/x-tcl",
+  der: "application/x-x509-ca-cert",
+  pem: "application/x-x509-ca-cert",
+  crt: "application/x-x509-ca-cert",
+  xpi: "application/x-xpinstall",
+  xhtml: "application/xhtml+xml; charset=utf-8",
+  zip: "application/zip",
+  bin: "application/octet-stream",
+  exe: "application/octet-stream",
+  dll: "application/octet-stream",
+  deb: "application/octet-stream",
+  dmg: "application/octet-stream",
+  eot: "application/octet-stream",
+  iso: "application/octet-stream",
+  img: "application/octet-stream",
+  msi: "application/octet-stream",
+  msp: "application/octet-stream",
+  msm: "application/octet-stream",
+  mid: "audio/midi",
+  midi: "audio/midi",
+  kar: "audio/midi",
+  mp3: "audio/mpeg",
+  ogg: "audio/ogg",
+  ra: "audio/x-realaudio",
+  '3gpp': "video/3gpp",
+  '3gp': "video/3gpp",
+  mpeg: "video/mpeg",
+  mpg: "video/mpeg",
+  mov: "video/quicktime",
+  flv: "video/x-flv",
+  mng: "video/x-mng",
+  asx: "video/x-ms-asf",
+  asf: "video/x-ms-asf",
+  wmv: "video/x-ms-wmv",
+  avi: "video/x-msvideo",
+  m4v: "video/mp4",
+  mp4: "video/mp5"
+};
+
+
+function getContentType(filename) {
+  const ext = (filename || '').match(/\.(\w+)$/);
+  return MIME_TYPES[ext ? ext[1] : 'html'];
+}
+
 function isIPAddress(str) {
   return /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(str);
 }
@@ -112,13 +213,23 @@ const server = http.createServer((req, res) => {
     return;
   }
 
+  res.writeHead(200, {
+    'Content-Type': getContentType(path)
+  });
+
   console.log(`Getting content for ${website}:${path}...`);
   req.on('err', err => {
   });
   req.on('data', chunk => {
   });
   req.on('end', () => {
-    getContent({clientId: config.dest[host].clientId, accessToken: config.dest[host].accessToken, host, website, path}).then(content => {
+    getContent({
+      clientId: config.dest[host].clientId,
+      accessToken: config.dest[host].accessToken,
+      host,
+      website,
+      path
+    }).then(content => {
       res.write(content);
       res.end();
     }, err => {
